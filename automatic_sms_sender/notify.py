ab43b8b0b68226efbbda5d0a4c543a56571d70d6
Mudge13 <129602587+Mudge13@users.noreply.github.com>
2023-12-26 02:03:30 +0530

    Delete automatic_sms_sender directory

--- automatic_sms_sender/notify.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-requests.post("https://ntfy.sh/mihir_topic",
-              data="OOGA BOOGA BOO BOO !!!".encode(encoding='utf-8'))
--- automatic_sms_sender/test.py
+++
@@ -1,4 +0,0 @@
-# +17623095535
-# Auth Token = f86982da674e0bc28e65e33b000e1c5d
-
-print     ("hello world")
